1f8823d172a031d7b2aeb58966832eaa8177aec7
Kims-club98 <naxen5611@naver.com>
2026-03-13 21:07:14 +0900

    260313 에누리닷컴 크롤링/google api 추가

--- Python/라이브러리/flet/flet youtube/02.basic_signup.py
+++ Python/라이브러리/flet/flet youtube/02.basic_signup.py
@@ -2,7 +2,8 @@
 
 import flet as ft
 from flet import TextField, Checkbox, ElevatedButton, Text, Row, Column
-from flet_core.control_event import ControlEvent
+# from flet_core.control_event import ControlEvent
+from flet import ControlEvent
 
 def main(page: ft.Page) -> None:
   page.title = 'signup'
